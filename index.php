2e4bfcb8cd5e9ea9a04c931a140b7aa776061452
StrongStas1 <94853383+StrongStas1@users.noreply.github.com>
2021-11-22 18:50:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Natalia Victorovna</title>
+<meta charset="utf-8">
+<meta name="description" content="englesh Natalia Victorovna.">
+<meta name="keywords" content="englesh teacher Natalia Victorovna">
+<link rel="icon" type="image/png" href="/img/meta/favicon/16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/img/meta/favicon/32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/img/meta/favicon/96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/img/meta/favicon/128.png" sizes="128x128">
+<link rel="icon" type="image/png" href="/img/meta/favicon/196.png" sizes="196x196">
+<link rel="apple-touch-icon" href="/img/meta/mobile/57.png" sizes="57x57">
+<link rel="apple-touch-icon" href="/img/meta/mobile/60.png" sizes="60x60">
+<link rel="apple-touch-icon" href="/img/meta/mobile/72.png" sizes="72x72">
+<link rel="apple-touch-icon" href="/img/meta/mobile/76.png" sizes="76x76">
+<link rel="apple-touch-icon" href="/img/meta/mobile/114.png" sizes="114x114">
+<link rel="apple-touch-icon" href="/img/meta/mobile/120.png" sizes="120x120">
+<link rel="apple-touch-icon" href="/img/meta/mobile/144.png" sizes="144x144">
+<link rel="apple-touch-icon" href="/img/meta/mobile/152.png" sizes="152x152">
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta property="og:locale" content="en_US">
+<meta property="og:url" content="https://rpcs3.net">
+<meta property="og:type" content="website">
+<meta property="og:title" content="RPCS3 Sony PlayStation 3 Emulator">
+<meta property="og:description" content="engleash">
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/152.png">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/152.png">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux and BSD.">
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v7">
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7">
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v7">
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v7">
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v7">
+<script data-cfasync="false" src="/lib/js/jquery.js"></script>
+<script data-cfasync="false" src="/lib/js/dark.js"></script>
+<script data-cfasync="false" src="/lib/js/main.js"></script>
+<script data-cfasync="false" src="/lib/js/jscookie.js"></script>
+<script data-cfasync="false" src="/lib/js/particles.js"></script>
+<script data-cfasync="false" src="/lib/js/particles-sel.js"></script>
+<noscript>
+<style>
+/*
+General:
+- Elements that fade in are not visible by default
+*/
+.fade-up-onstart {
+	display:inherit;
+}
+.fade-in-onload {
+	display:inherit;
+}
+/*
+Index:
+- Center index contents
+*/
+.landing-con-container {
+	top:50%;
+	margin-top:-139px;
+}
+/*
+Quickstart:
+- Remove dimming
+- Do not block interaction outside the sidebar
+- Slightly reduce the width of the sidebar
+*/
+.toggle-navsidebar {
+	background:none;
+	width:auto;
+}
+.sidebar-con-anim {
+	width:300px;
+}
+</style>
+</noscript>
+<script data-cfasync="false" src="/lib/js/carousel.js?v=1"></script>
+</head>
+<body>
+<div class="page-con-content landing-con-override">
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="landing-con-container scale-content-txt-2 fade-in-onload">
+					<div class='landing-tx1-heading pulsate'>
+						<h2 align="center">Shkondina Natalia Victorovna<br>
+							Teacher English language </h2>
+								</div>
+									<div
+										class='landing-tx1-heading pulsate'>
+											<h1 align="center">My <a href="https://лвуфк.школа-лнр.рф"target="_blank">scholl </a></h1>
+											</div>
+										<div style="font-family:Courier, monospace">
+									<span> 
+								 
+							</span>
+						</div>
+					</div>
+				<div class='landing-con-right content-remove '>
+					<div class="MMImage-Origin">
+					 <img src="https://catherineasquithgallery.com/uploads/posts/2021-02/1613504405_41-p-fon-dlya-prezentatsii-uchitelya-angliiskog-49.png" style="width: 139%; height: 100%;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				<div class="search-con-inner">
+					<div class="search-inp-search darkmode-search-border">
+						<div class="search-ico-search darkmode-invert">
+						 </div>
+						       <form action='/compatibility' method='get'>
+							<input name="g" placeholder=" site search / search">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-video darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-btn-left darkmode-arrow-left left" href="#myCarousel" data-slide="prev">
+			</div>
+			<div class="content-btn-right darkmode-arrow-right right" href="#myCarousel" data-slide="next">
+			</div>
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>article</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div
+	<p><a href="natali.shkondina@yandex.ru">Задавайте вопросы по электронной почте</a></p>
+		</div>
+	<div
+		<div class="landing-con-discord darkmode-discord">
+			<div class="discord-left-logo">
+				<img src="/img/graphics/svg/discord-left.svg" style="width: 80%; height: 80%;">
+			</div
+			<div class="discord-center-logo">
+				<img src="/img/graphics/svg/discord-center.svg" style="width: 80%; height: 80%;">
+			</div>
+				<div class='landing-con-left content-remove '>
+		</div>	
+	<div class="content-con-inside">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+			</div>
+		</div>
+	</div>
+	
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Advertisement</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.gooрglesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>Support Us</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 Our developers are pushing the very boundaries of PlayStation 3 emulation. Our rapid progression would not be possible without our contributors and Patrons. Our patrons not only keep us motivated, but keep our lead developers working on the project full time.
+								</p>
+							</div>
+							<a href="https://www.patreon.com/Nekotekina" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>Become a Patron</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
